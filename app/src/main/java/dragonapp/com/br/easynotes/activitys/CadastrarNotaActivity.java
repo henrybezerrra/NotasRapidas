506ef30fd829c2de05ce3df67f31b368e02a3ef5
Murilo Henrique Bezerra Silva <henrybezerra10@outlook.com>
2017-06-05 11:45:26 -0300

    Mudança no xml, criacao dos codigos do CadastrarNotaActivity e MainActivity. A MainActivity ainda nao foi renomeada.

--- app/src/main/java/dragonapp/com/br/easynotes/activitys/CadastrarNotaActivity.java
+++ app/src/main/java/dragonapp/com/br/easynotes/activitys/CadastrarNotaActivity.java
@@ -1,15 +1,75 @@
 package dragonapp.com.br.easynotes.activitys;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import dragonapp.com.br.easynotes.R;
+import dragonapp.com.br.easynotes.dao.Nota;
+import dragonapp.com.br.easynotes.dao.NotaDAO;
 
 public class CadastrarNotaActivity extends AppCompatActivity {
+
+    private static Nota nota;
+    public static void chamaTela(Context context, Nota nota) {
+        CadastrarNotaActivity.nota = nota;
+        Intent intent = new Intent(context, CadastrarNotaActivity.class);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_nota);
+
+        final EditText edtNomedaNota = (EditText) findViewById(R.id.edtNome);
+        final EditText edtDescricaodaNota = (EditText) findViewById(R.id.edtDescricao);
+        Button btSalvarNota = (Button) findViewById(R.id.btSalvar);
+
+        if (nota != null) {
+            edtNomedaNota.setText(nota.getNome());
+        }
+
+        btSalvarNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edtNomedaNota.getText().toString().isEmpty()||edtDescricaodaNota.getText().toString().isEmpty()){
+                    Toast.makeText(CadastrarNotaActivity.this, "Preencha todos os campos!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    NotaDAO notaDAO = new NotaDAO(CadastrarNotaActivity.this);
+                    if(nota==null) {
+                        nota = new Nota();
+                    }
+
+                    nota.setNome(edtNomedaNota.getText().toString());
+                    nota.setDescricao(edtDescricaodaNota.getText().toString());
+                    if (nota.getId()==0) {
+                        notaDAO.salvar(nota);
+                    }else {
+                        notaDAO.atualizar(nota);
+                    }
+                    Toast.makeText(CadastrarNotaActivity.this, "Nota Salva!",
+                            Toast.LENGTH_SHORT).show();
+                    edtNomedaNota.setText("");
+                    edtDescricaodaNota.setText("");
+
+                }
+            }
+        });
+
+        /*btListarCidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CidadeActivity.this, ListaCidadeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }*/
     }
 }
--- app/src/main/java/dragonapp/com/br/easynotes/activitys/MainActivity.java
+++ app/src/main/java/dragonapp/com/br/easynotes/activitys/MainActivity.java
@@ -1,15 +1,71 @@
 package dragonapp.com.br.easynotes.activitys;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import dragonapp.com.br.easynotes.R;
+import dragonapp.com.br.easynotes.dao.Nota;
+import dragonapp.com.br.easynotes.dao.NotaDAO;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lista = (ListView) findViewById(R.id.lvLista);
+        atualizarListaDeNotas();
+        lista.setOnItemClickListener(this);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+        final Nota nota = (Nota) parent.getAdapter().getItem(position);
+        final NotaDAO dao = new NotaDAO(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Opções");
+        builder.setMessage("O que deseja fazer?");
+        builder.setPositiveButton("Editar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                CadastrarNotaActivity.chamaTela(MainActivity.this,nota);
+
+            }
+        });
+
+        builder.setNegativeButton("Exclir", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dao.deletar(Nota.getId());
+                Toast.makeText(MainActivity.this, "Nota "+Nota.getNome()+", excluído com sucesso!",
+                        Toast.LENGTH_LONG).show();
+                atualizarListaDeNotas();
+            }
+        });
+        AlertDialog alert;
+        alert = builder.create();
+        alert.show();
+
+
+    }
+    private void atualizarListaDeNotas(){
+        NotaDAO dao = new NotaDAO(this);
+        ArrayAdapter<Nota> arrayAdapter = new ArrayAdapter<Nota>(this,
+                android.R.layout.simple_list_item_1, dao.listar());
+
+        lista.setAdapter(arrayAdapter);
 }
