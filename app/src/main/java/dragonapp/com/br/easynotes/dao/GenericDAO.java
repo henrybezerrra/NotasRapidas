1666a8f60aff297a92cff8c4c09e8555cd01014f
Murilo Henrique Bezerra Silva <henrybezerra10@outlook.com>
2017-06-05 11:18:04 -0300

    Criação do BD e metodos salva, atualizar, lista e deletar

--- app/src/main/java/dragonapp/com/br/easynotes/dao/GenericDAO.java
+++ app/src/main/java/dragonapp/com/br/easynotes/dao/GenericDAO.java
@@ -1,5 +1,7 @@
 package dragonapp.com.br.easynotes.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
@@ -8,4 +10,30 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public abstract class GenericDAO extends SQLiteOpenHelper {
+
+
+    private static final String NOME_BANCO = "unileao_manhaNota";
+    private static final int VERSAO = 3;
+    private String sqlTabelaNota = "CREATE TABLE IF NOT EXISTS nota(" +
+            "idnota INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "nome VARCHAR(45) NOT NULL," +
+            "descricao VARCHAR(45) NOT NULL," +
+            ");";
+
+
+    public GenericDAO(Context context){
+        super(context, NOME_BANCO, null, VERSAO);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sqlTabelaNota);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
 }
--- app/src/main/java/dragonapp/com/br/easynotes/dao/NotaDAO.java
+++ app/src/main/java/dragonapp/com/br/easynotes/dao/NotaDAO.java
@@ -1,16 +1,70 @@
 package dragonapp.com.br.easynotes.dao;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Murilo Henrique on 31/05/2017.
  */
 
-public class NotaDAO extends GenericDAO implements DAO<T> {
+public class NotaDAO extends GenericDAO implements DAO<Nota> {
 
     private SQLiteDatabase dataBase;
 
-    public NotaDAO(){
+    public NotaDAO(Context context){
+        super(context);
+        dataBase = getWritableDatabase();
 
+    }
+
+    @Override
+    public boolean salvar(Nota nota) {
+        dataBase.execSQL("INSERT INTO nota(nome, descricao)" +
+                        " VALUES(?,?)",
+                new Object[] {nota.getNome(), nota.getDescricao()});
+        return false;
+
+    }
+
+    @Override
+    public List<Nota> listar() {
+        Cursor cursor = dataBase.rawQuery("SELECT * FROM nota", null);
+        List<Nota> notas = new ArrayList<>();
+        if(cursor!=null){
+            if(cursor.getCount()>0){
+                int idxCodigo = cursor.getColumnIndex("idnota");
+                int idxNome = cursor.getColumnIndex("nome");
+                int idxDescricao = cursor.getColumnIndex("descricao");
+                cursor.moveToFirst();
+                do{
+                    Nota nota = new Nota();
+                    nota.setId(cursor.getInt(idxCodigo));
+                    nota.setNome(cursor.getString(idxNome));
+                    nota.setDescricao(cursor.getString(idxDescricao));
+
+                    notas.add(nota);
+
+                }while(cursor.moveToNext());
+            }
+        }
+        return notas;
+    }
+
+    @Override
+    public boolean deletar(int id) {
+        dataBase.execSQL("DELETE FROM nota WHERE idnota=?",
+                new Object[]{id});
+        return false;
+    }
+
+    @Override
+    public boolean atualizar(Nota nota) {
+        dataBase.execSQL("UPDATE nota SET nome=?, descricao=?" +
+                        " WHERE idnota=?",
+                new Object[]{nota.getNome(), nota.getDescricao(), nota.getId()});
+        return false;
     }
 }
