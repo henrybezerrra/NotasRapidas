48dfdff95b98daaf5909f602978b6b8752c9240d
Murilo Henrique Bezerra Silva <henrybezerra10@outlook.com>
2017-06-07 08:58:48 -0300

    correção

--- app/src/main/java/dragonapp/com/br/easynotes/activitys/MainActivity.java
+++ app/src/main/java/dragonapp/com/br/easynotes/activitys/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dao.deletar(nota.getId());
-                Toast.makeText(MainActivity.this, "Cliente "+nota.getNome()+", excluído com sucesso!",
+                Toast.makeText(MainActivity.this, "Nota "+nota.getNome()+", excluído com sucesso!",
                         Toast.LENGTH_LONG).show();
                 atualizarListaNota();
             }
